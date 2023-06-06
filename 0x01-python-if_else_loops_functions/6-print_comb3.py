556f2d0cc94cb6fb3e708c7be54276276e27726a
Siphiwe <nompychiloane@gmail.com>
2023-06-06 11:59:50 -0700

    prints all possible different combinations of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 6-print_comb3.py
+#6-print_comb3.py
 
 """Print all possible different combinations of two digits in ascending order.
 The two digits must be different -01 and 10 are considered identical
