d43452db4e9477d3c2410fa76a32ce822cc51319
Siphiwe <nompychiloane@gmail.com>
2023-07-12 05:23:31 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,18 +3,22 @@
 import json
 import sys
 
+
 def save_to_json_file(data, filename):
+
     with open(filename, 'w') as file:
         json.dump(data, file)
 
+
 def load_from_json_file(filename):
+
     with open(filename, 'r') as file:
         return json.load(file)
+
 
 if __name__ == "__main__":
     try:
         items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
