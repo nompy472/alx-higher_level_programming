e3abbe5c956a4febfd76648907c7707dd4c2b992
Siphiwe <Nompychiloane@gmail.com>
2023-11-30 21:52:43 -0800

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the specified URL and displays the body of the response for a 200 status code.
-curl -s $1
+curl -s "$1" -o /dev/null -w "%{http_code}\n" | [ "$(cat)" == "200" ] && curl -s "$1"
